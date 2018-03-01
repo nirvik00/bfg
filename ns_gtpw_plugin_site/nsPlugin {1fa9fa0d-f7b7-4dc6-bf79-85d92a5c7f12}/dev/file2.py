fed2e4165def5a8c78dc1db4b30ed49d513abb89
Nirvik Saha <nirviksaha@gatech.edu>
2018-03-01 11:56:04 -0500

    Update file2.py

--- ns_gtpw_plugin_site/nsPlugin {1fa9fa0d-f7b7-4dc6-bf79-85d92a5c7f12}/dev/file2.py
+++ ns_gtpw_plugin_site/nsPlugin {1fa9fa0d-f7b7-4dc6-bf79-85d92a5c7f12}/dev/file2.py
@@ -27,15 +27,15 @@ class RunProc(object):
         if(n==0 or n==None):
             n=1
         FileName="input_1.csv"
-        FilePath=rs.GetString("Enter the working directory for the program : ")
-        if(FilePath==""):
-            FilePath='c:/nir_dev/temp/output_1'
+        self.FilePath=rs.GetString("Enter the working directory for the program : ")
+        if(self.FilePath==""):
+            self.FilePath='c:/nir_dev/temp/output_1'
         try:
-            os.stat(FilePath)
+            os.stat(self.FilePath)
         except:
             print('folder does not exist')
             return
-        os.chdir(FilePath)
+        os.chdir(self.FilePath)
         req_fsr=3.0
 
         a=int(math.sqrt(n))
@@ -100,15 +100,6 @@ class RunProc(object):
                 dy=pt[1]
                 v=[dx,dy,0]
                 srf_obj=m.finalSrf()
-                if(n==1):
-                    self.output3sync(srf_obj,rs.CurvePoints(temp_site_crv),v,k)
-                else:
-                    self.output3mass(srf_obj,rs.CurvePoints(temp_site_crv),v,k)
-                try:
-                    file_path='c:/nir_dev/temp/output_1'
-                    os.chdir(file_path)
-                except:
-                    pass
                 #end 3 connections
                 self.addLabel(k,temp_site_crv)
                 self.req_srfobj_li.append(m.finalSrf())
@@ -146,40 +137,9 @@ class RunProc(object):
                 if(d>max):
                     max=d
         return max
-    
-    def writeToCsvPcp(self, counter, str_li):
-        FilePath='c:/nir_dev/temp/output_1'
-        try:
-            os.stat(FilePath)
-        except:
-            print('2folder does not exist')
-            return        
-        file='output'+str(time())+".csv"
-        fs=open(file,"a")
-        site_area=rs.CurveArea(self.site_crv)[0]
-        fs.write("Run Id,GFA,FSR,Ground Coverage,Num of Floors, Num Plotted, Area of Floor Plate, Image (PNG)\n")
-        for i in range(counter):#2-d str list
-            total_area=0
-            gc=0
-            num_flrs=0
-            num_plot=0
-            area_plate=0
-            for j in str_li[i]:#1-d element of str list
-                total_area+=j[2]*j[3]*j[4]
-                gc+=j[2]*j[4]
-                num_flrs+=(j[1])
-                num_plot+=(j[3])
-                area_plate+=(j[4])
-            run_id=i+1
-            gfa=total_area
-            gc=gc*100/site_area
-            fsr=total_area/site_area
-            img=str(i+1)+'.png'
-            fs.write(str(run_id)+","+str(gfa)+","+str(fsr)+","+str(gc)+","+str(num_flrs)+","+str(num_plot)+","+str(area_plate)+","+img+"\n")
-        fs.close()
-
+   
     def writeToCsv(self, counter, str_li):
-        FilePath='c:/nir_dev/temp/output_1'
+        FilePath=self.FilePath
         try:
             os.stat(FilePath)
         except:
@@ -220,86 +180,6 @@ class RunProc(object):
         r=self.res_obj[best_index]
         loc_pt=self.loc_pts[best_index]
         rs.AddCircle(loc_pt,(2+self.max)/2)
-        
-    def output3sync(self, srf_obj,pts,v,k):
-        FilePath='c:/nir_dev/web/dennis/proj1/public/csv'
-        try:
-            os.stat(FilePath)
-        except:
-            print('0folder does not exist')
-            return
-        os.chdir(FilePath)
-        try:
-            os.remove('output3.csv')
-        except OSError:
-            pass
-        file='output3'+".csv"
-        fs=open(file,"w")
-        site_pts=[]
-        p=""
-        for i in pts:
-            x=i[0]-v[0]
-            y=i[1]-v[1]
-            p+=(str(x)+";"+str(y)+",")
-        fs.write(p)
-        fs.write("\n")
-        s=[] 
-        geo_di={}
-        for i in srf_obj:
-            j=i[0].getName()
-            srf=i[1]
-            b=rs.BoundingBox(srf)
-            x=b[0][0]-v[0]
-            y=b[0][1]-v[1]
-            z=0
-            l=rs.Distance(b[0],b[1])
-            w=rs.Distance(b[1],b[2])
-            h=rs.Distance(b[3],b[7])
-            str0=j+","+str(x)+","+str(y)+","+str(z)+","+str(l)+","+str(w)+","+str(h)
-            s.append(str0)
-            fs.write(str0)
-            fs.write("\n")
-        fs.close()
-        
-    def output3mass(self, srf_obj,pts,v,k):
-        FilePath='c:/nir_dev/spatial_allocation/bfg_output/csv'
-        try:
-            os.stat(FilePath)
-        except:
-            print('0folder does not exist')
-            return
-        os.chdir(FilePath)
-        try:
-            os.remove('output3.csv')
-        except OSError:
-            pass
-        file='output'+str(k)+".csv"
-        fs=open(file,"w")
-        site_pts=[]
-        p=""
-        for i in pts:
-            x=i[0]-v[0]
-            y=i[1]-v[1]
-            p+=(str(x)+";"+str(y)+",")
-        fs.write(p)
-        fs.write("\n")
-        s=[] 
-        geo_di={}
-        for i in srf_obj:
-            j=i[0].getName()
-            srf=i[1]
-            b=rs.BoundingBox(srf)
-            x=b[0][0]-v[0]
-            y=b[0][1]-v[1]
-            z=0
-            l=rs.Distance(b[0],b[1])
-            w=rs.Distance(b[1],b[2])
-            h=rs.Distance(b[3],b[7])
-            str0=j+","+str(x)+","+str(y)+","+str(z)+","+str(l)+","+str(w)+","+str(h)
-            s.append(str0)
-            fs.write(str0)
-            fs.write("\n")
-        fs.close()
 
 
 if __name__=='__main__':
